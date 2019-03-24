c5c9aed13f6cdc8bab1799aabf5f3ee4f6326306
Oreofe Olurin <oreofeolurin@gmail.com>
2019-03-24 13:04:26 +0100

    feat(): added donation component to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {AuthGuard} from './core/guards/auth.guard';
 import {HttpClientModule} from '@angular/common/http';
+import { DonationComponent } from './features/donation/donation.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        DonationComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         RouterModule.forRoot([
+            {path: 'donation', component: DonationComponent},
             {path: 'cars', loadChildren: './features/car/car.module#CarModule', canLoad: [AuthGuard]},
             {path: 'trucks', loadChildren: './features/truck/truck.module#TruckModule', canLoad: [AuthGuard]},
         ]),
